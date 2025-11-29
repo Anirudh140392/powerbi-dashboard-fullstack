2fa0b064bbf05ee5dae59e4f09dd2289707b0c3a
0shubham11 <shubham.s1@trailytics.com>
2025-11-29 08:45:23 +0530

    all change done regarding setup of tailwind css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { CssBaseline } from "@mui/material";
 import App from "./App";
 import AppThemeProvider from "./utils/ThemeContext";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
