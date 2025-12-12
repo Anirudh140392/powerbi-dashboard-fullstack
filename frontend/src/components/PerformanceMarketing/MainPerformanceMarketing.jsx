1beb858660d81df8157ebad5e62849d1f4b063e2
0shubham11 <shubham.s1@trailytics.com>
2025-12-12 17:35:48 +0530

    all changes done

--- frontend/src/components/PerformanceMarketing/MainPerformanceMarketing.jsx
+++ frontend/src/components/PerformanceMarketing/MainPerformanceMarketing.jsx
@@ -8,6 +8,7 @@ import HeatMapDrillTable from "./HeatMapDrillTable";
 import InsightHorizontalKpis from "./InsightHorizontalKpis";
 import KeywordAnalysisTable from "./KeywordAnalysisTable";
 import MetricCardContainer from "../CommonLayout/MetricCardContainer";
+import DrilldownLatestTable from "./DrilldownLatestTable";
 
 const cards = [
   {
@@ -75,66 +76,12 @@ export default function MainPerformanceMarketings() {
       }
     };
 
-    fetchPerformanceData();
-  }, []);
+    fetchPerformanceData()
+  }, [])
 
   return (
     <Box>
-      {/* <Box>
-        <Card
-          sx={{
-            p: 3,
-            borderRadius: 3,
-            border: "1px solid #e2e8f0",
-          }}
-        >
-          <Typography variant="h6" fontWeight={700}>
-            kpis overview
-          </Typography>
-          <Box sx={{ height: 1, borderBottom: "1px solid #e5e7eb", my: 2 }} />
-          <Grid container spacing={3}>
-            {performanceData.kpi_cards.map((kpi, i) => {
-              const Icon = Icons[kpi.icon];
-              return (
-                <Grid item xs={12} sm={6} md={3} key={i}>
-                  <Card
-                    sx={{
-                      p: 3,
-                      height: 135,
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "space-between",
-                      transition: "0.3s",
-                      "&:hover": {
-                        transform: "translateY(-6px)",
-                        boxShadow: "0 12px 35px rgba(0,0,0,0.12)",
-                      },
-                    }}
-                  >
-                    <Box display="flex" justifyContent="space-between">
-                      <Typography variant="subtitle2" color="text.secondary">
-                        {kpi.label}
-                      </Typography>
-                      <Icon size={22} color="#6366F1" />
-                    </Box>
-
-                    <Typography variant="h4" fontWeight={700}>
-                      {kpi.value}
-                    </Typography>
-
-                    <Chip
-                      label={`${kpi.change} vs last month`}
-                      color={kpi.positive ? "success" : "error"}
-                      size="small"
-                      sx={{ alignSelf: "flex-start" }}
-                    />
-                  </Card>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Card>
-      </Box> */}
+ 
       <Box sx={{ mt: 4 }}>
         <MetricCardContainer
           title="Performance Marketing Overview"
@@ -154,6 +101,9 @@ export default function MainPerformanceMarketings() {
       <Box sx={{ mt: 4 }}>
         <KeywordAnalysisTable />
       </Box>
+      <Box sx={{ mt: 4 }}>
+        <DrilldownLatestTable />
+      </Box>
     </Box>
   );
-}
+};
