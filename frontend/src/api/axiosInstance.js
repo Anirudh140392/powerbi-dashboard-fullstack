50c90e84304e72962beccd60967a8c9f9e38c9bc
KenilKavar <kenil.k@trailytics.com>
2026-01-14 13:41:14 +0530

    Deployed

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "http://3.7.138.75:5001/api",
-    baseURL: "http://localhost:5000/api",
+    // For production: use relative URL (nginx proxies /api to backend)
+    baseURL: "/api",
 
     headers: {
         "Content-Type": "application/json",
