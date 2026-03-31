768d18b32013fbe7b4cc09f411073547683b9197
0shubham11 <shubham.s1@trailytics.com>
2026-03-31 13:59:44 +0530

    changes done

--- frontend/src/pages/Admin/AdminPanel.jsx
+++ frontend/src/pages/Admin/AdminPanel.jsx
@@ -22,7 +22,7 @@ const AdminPanel = () => {
     const { logout, user } = useAuth();
 
     const menuItems = [
-        { id: "dashboard", label: "Dashboard", icon: LayoutDashboard },
+        // { id: "dashboard", label: "Dashboard", icon: LayoutDashboard },
         { id: "users", label: "User Management", icon: Users },
         // { id: "roles", label: "Permissions", icon: ShieldAlert },
         // { id: "access", label: "Access Mapping", icon: Network },
