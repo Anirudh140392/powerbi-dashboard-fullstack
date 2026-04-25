254c1668cc2fab918eb077947344dedf5d0a64c3
KenilKavar <kenil.k@trailytics.com>
2026-04-25 14:33:18 +0530

    Update Segment names and added segment

--- backend/test.js
+++ backend/test.js
@@ -1,22 +1,12 @@
-const axios = require('axios');
-async function run() {
-    try {
-        const query = `
-            SELECT 
-                keyword_search_product as sku,
-                topKIf(1)(POSITION, spons = '1') as ad_pos_arr,
-                topKIf(1)(POSITION, organic = '1') as org_pos_arr,
-                topKIf(1)(POSITION, overall = '1') as overall_pos_arr,
-                topKIf(1)(brand_name_th, brand_name_th != '') as brand_arr
-            FROM rb_kw_olap
-            WHERE keyword_search_product != '' AND POSITION < 11
-            GROUP BY sku
-            LIMIT 5
-        `;
-        const res = await axios.post('http://localhost:8123/', query, {
-            auth: { username: 'default', password: '' }
-        });
-        console.log(res.data);
-    } catch(err) { console.error(err.response ? err.response.data : err.message); }
+const { ClickHouse } = require('clickhouse');
+const clickhouse = new ClickHouse({
+  url: 'http://216.48.177.108',
+  port: 8123,
+  debug: false,
+  basicAuth: { username: 'default', password: '' }
+});
+async function main() {
+  const rs = await clickhouse.query('DESCRIBE TABLE demo.rb_ms_olap FORMAT JSONEachRow').toPromise();
+  console.log(rs.slice(0, 15));
 }
-run();
+main();
