89295c01984cab881beab9097cbdae41c07c26ab
KenilKavar <kenil.k@trailytics.com>
2026-06-03 10:21:41 +0530

    Update Notification page wise

--- backend/test-dayjs.js
+++
@@ -1,5 +0,0 @@
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat.js';
-dayjs.extend(customParseFormat);
-
-console.log(dayjs("2026-03-01", ['YYYY-MM-DD', 'DD-MM-YYYY']).format('YYYY-MM-DD'));
